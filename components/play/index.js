d4cec2dbec153b87bdaee7830e9c83d91edb415a
Tendeeno <95103105+Tendeeno@users.noreply.github.com>
2023-02-10 10:32:43 -0500

    add very ruf game docs

--- components/play/index.js
+++ components/play/index.js
@@ -15,17 +15,20 @@ const Play = () => {
   return (
     <div className="w-full min-h-[546px] h-full">
       <div className="flex items-start justify-center w-full h-full pt-10">
-        <div className="relative w-[276px]">
-          {insertCoin && (
-            <div className="absolute z-20 w-[200px] h-[200px] top-20 animation-insert-coin">
-              <CantoCoin />
+        <div className="flex justify-between w-full h-full min-h-full p-12 bg-brand-black">
+          <h1 className="text-brand-green">Want to play a game?</h1>
+          <div className="relative w-[276px] h-[348px]">
+            {insertCoin && (
+              <div className="absolute z-20 w-[200px] h-[200px] top-20 animation-insert-coin">
+                <CantoCoin />
+              </div>
+            )}
+            <div
+              className="absolute w-[276px] cursor-pointer"
+              onClick={toggleCoin}>
+              <CantoPlayBg className="absolute top-0 right-0 z-10" />
+              <CantoPlay className="absolute top-0 right-0 z-30" />
             </div>
-          )}
-          <div
-            className="absolute w-[276px] cursor-pointer"
-            onClick={toggleCoin}>
-            <CantoPlayBg className="absolute top-0 right-0 z-10" />
-            <CantoPlay className="absolute top-0 right-0 z-30" />
           </div>
         </div>
       </div>
